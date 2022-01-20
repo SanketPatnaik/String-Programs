9e1ace309e264e1136d9ccca68009367e0527597
Sanket Patnaik <97582374+SanketPatnaik@users.noreply.github.com>
2022-01-20 12:41:32 +0530

    Add files via upload

---
+++ StringCharacterPrint.java
@@ -0,0 +1,13 @@
+package com.in28minutes.ReferenceDataTypes;
+
+public class StringCharacterPrint {
+	public void printStringCharacters() {
+		String str = "Neetu is so Beautiful";
+
+		for (int i = 0; i <= 20; i++) {
+			System.out.print(str.charAt(i));
+		}
+
+	}
+
+}
---
+++ StringCharacterPrintRunner.java
@@ -0,0 +1,13 @@
+package com.in28minutes.ReferenceDataTypes;
+
+public class StringCharacterPrintRunner {
+
+	public static void main(String[] args) {
+	StringCharacterPrint string = new StringCharacterPrint();
+	string.printStringCharacters ();
+
+
+
+	}
+
+}
